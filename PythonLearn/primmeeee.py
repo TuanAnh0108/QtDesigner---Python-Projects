81afa726ce09d6c102712df0f9280b5c05862588
TuanAnh0108 <57340581+TuanAnh0108@users.noreply.github.com>
2020-02-12 08:45:48 +0900

    Update primmeeee.py

--- PythonLearn/primmeeee.py
+++ PythonLearn/primmeeee.py
@@ -3,14 +3,17 @@ import time
 import math
 
 
-def is_Prime_v2(n):
+def is_Prime(n):
+    if n == 2:
+        return True
+    if n > 2 and n % 2 == 0:
+        return False
     if n == 1: 
         return False
+
     max_divisor = math.floor(math.sqrt(n))
-    for d in range(2, 1 + int(max_divisor)):
+    for d in range(3, 1 + int(max_divisor),2):
         if n % d == 0:
             return False
     return True
 
-for n in range(1, 21):
-    print(n, is_Prime_v2(n))
