3d898fd371a19fc491dd450a09be692d894474f3
TuanAnh0108 <57340581+TuanAnh0108@users.noreply.github.com>
2020-02-20 20:55:03 +0900

    Update graphs.py

--- PythonLearn/graphs.py
+++ PythonLearn/graphs.py
@@ -1,4 +1,19 @@
-1.
+1. f(x) = (x+1)^2 - 1, with x from -2, to 2 with 1000 points
+
+import matplotlib.pyplot as pyplot
+import math
+x = []
+y = []
+min = -2
+
+for i in range(1000):
+    x.append(min + 0.004 * (i+1))
+    y.append((x[i]+1)**2 - 1)
+
+pyplot.plot(x, y)
+pyplot.xlabel('x')
+pyplot.ylabel('y = 0.1 * sin(0.1 * m(x))')
+
 2. [HL] g(x) = 0.1*sin(0.1*m(x)), where m(x) = x^2, and x from 0 to 30 with steps of 0.05
 
 import matplotlib.pyplot as pyplot
